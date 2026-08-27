8f5e8e9d32264f339e0b3b5f9cc4fe294561bd13
Bindu Sree D <bindusreedandu@gmail.com>
2026-08-27 21:49:39 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000"
+  baseURL: "https://cybershield-ai-zn81.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
